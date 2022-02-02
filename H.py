2627518fa03160538c66e9882bc58f6eee64fa7b
Kamila <98703242+SakushK@users.noreply.github.com>
2022-02-03 02:22:25 +0600

    Add files via upload

---
+++ H.py
@@ -0,0 +1,24 @@
+s=input()
+d=input()
+result=""
+
+first=s.find(d)
+
+if(not(first==-1)):
+    result+=str(first)
+    
+    last=s.rfind(d)
+    if(last==first):
+        print(result)
+        quit()
+    
+        
+        
+    else:
+        result+=" "
+        result+=str(last)
+        
+        if len(result)!=0:
+            print(result)
+        
+        
