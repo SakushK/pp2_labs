305f4553c9c85af64e05da7f96128244f5b77797
Kamila <98703242+SakushK@users.noreply.github.com>
2022-02-18 16:06:55 +0600

    Create 1.py

---
+++ lab3/classes/1.py
@@ -0,0 +1,13 @@
+class MyClass:
+   def __init__(s):
+        s.string= ""
+
+   def getString(s):
+        s.string=input()
+
+   def printString(s):
+        print(s.string.upper())
+        
+d=MyClass()  
+d.getString()
+d.printString()
