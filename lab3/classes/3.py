615f3922e74e6e2cb40ec512698cab309ab9e6c7
Kamila <98703242+SakushK@users.noreply.github.com>
2022-02-18 21:01:18 +0600

    Create 3.py

---
+++ lab3/classes/3.py
@@ -0,0 +1,21 @@
+class Shape(object):
+    def __init__(self):
+        pass
+
+    def area(self):
+        return 0
+    
+class Rectangle(Shape):
+    def __init__(self, l, w):
+        self.length = l
+        self.width  = w
+
+    def area(self):
+        return self.length*self.width
+    
+
+a=int(input())
+b=int(input())
+
+result = Rectangle(a,b)
+print(result.area())
