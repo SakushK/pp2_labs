20632a46e09a08da71cf97eda1f5f6c9e4bdb447
Kamila <98703242+SakushK@users.noreply.github.com>
2022-02-18 21:52:20 +0600

    Create 3.py

---
+++ lab3/functions2/3.py
@@ -0,0 +1,93 @@
+movies = [
+{
+"name": "Usual Suspects", 
+"imdb": 7.0,
+"category": "Thriller"
+},
+{
+"name": "Hitman",
+"imdb": 6.3,
+"category": "Action"
+},
+{
+"name": "Dark Knight",
+"imdb": 9.0,
+"category": "Adventure"
+},
+{
+"name": "The Help",
+"imdb": 8.0,
+"category": "Drama"
+},
+{
+"name": "The Choice",
+"imdb": 6.2,
+"category": "Romance"
+},
+{
+"name": "Colonia",
+"imdb": 7.4,
+"category": "Romance"
+},
+{
+"name": "Love",
+"imdb": 6.0,
+"category": "Romance"
+},
+{
+"name": "Bride Wars",
+"imdb": 5.4,
+"category": "Romance"
+},
+{
+"name": "AlphaJet",
+"imdb": 3.2,
+"category": "War"
+},
+{
+"name": "Ringing Crime",
+"imdb": 4.0,
+"category": "Crime"
+},
+{
+"name": "Joking muck",
+"imdb": 7.2,
+"category": "Comedy"
+},
+{
+"name": "What is the name",
+"imdb": 9.2,
+"category": "Suspense"
+},
+{
+"name": "Detective",
+"imdb": 7.0,
+"category": "Suspense"
+},
+{
+"name": "Exam",
+"imdb": 4.2,
+"category": "Thriller"
+},
+{
+"name": "We Two",
+"imdb": 7.2,
+"category": "Romance"
+}
+]
+
+def movies_category(category):
+    movies_category = []
+    for m in movies:
+        if m["category"] == category:
+            movies_category.append(m["name"])
+    return movies_category
+
+romance_movies = movies_category("Romance")
+thriller_movies = movies_category("Thriller")
+suspence_movies = movies_category("Suspense")
+
+
+print (romance_movies)
+print(thriller_movies)
+print(suspence_movies)
