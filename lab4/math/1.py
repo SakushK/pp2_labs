34a653c1b238fed01b0d505d7f2b1e407a56a919
Kamila <98703242+SakushK@users.noreply.github.com>
2022-03-04 14:54:09 +0600

    Create 1.py

---
+++ lab4/math/1.py
@@ -0,0 +1,15 @@
+import math
+
+def radian(n):
+    
+    result=math.radians(n)
+    
+    return result
+
+"""in math function pi=3.14159,
+but in github case pi=22/7=3.14285
+that's why answers are different"""
+
+n=int(input())
+
+print(radian(n))
