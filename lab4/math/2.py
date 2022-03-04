6e62e6a4e131cd78887cfba8b6bce4ecfe62b71b
Kamila <98703242+SakushK@users.noreply.github.com>
2022-03-04 14:54:39 +0600

    Create 2.py

---
+++ lab4/math/2.py
@@ -0,0 +1,15 @@
+def area_trapezoid(height,base1,base2):
+    
+    A=float(((base1+base2)*height)/2)
+    
+    print (A)
+    
+h=int(input())
+
+a=int(input())
+
+b=int(input())
+
+area_trapezoid(h,a,b)
+
+
