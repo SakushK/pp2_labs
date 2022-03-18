c7d59e6a48d0d20360771404b61fc2a9e73a74e0
Kamila <98703242+SakushK@users.noreply.github.com>
2022-03-18 16:40:11 +0600

    Delete 4.py

--- lab6/4.py
+++
@@ -1,9 +0,0 @@
-from time import sleep
-import math 
-n = int(input("Sample Input:"))
-
-t = int(input())
-sleep(t/1000)
-
-print("Sample Output:")
-print("Square root of " + str(n) +  " after "+ str(t)+" miliseconds is "+ str(math.sqrt(n)))
